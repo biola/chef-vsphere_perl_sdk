fe806359845fcebc40d4bdfdd0bded5f611f76b9
troyready <troy@troyready.com>
2014-12-11 15:15:21 -0800

    add source/issue url to metadata & relax deps

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'troy.ready@biola.edu'
 license          'Apache 2.0'
 description      'Deploys the VMware vSphere Perl SDK'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+source_url       'https://github.com/biola/chef-vsphere_perl_sdk'
+issues_url       'https://github.com/biola/chef-vsphere_perl_sdk/issues'
+version          '1.0.1'
 
-depends          'ark', '~> 0.9.0'
-depends          'apt', '~> 2.5.0'
+depends          'ark', '~> 0.9'
+depends          'apt', '~> 2.5'
